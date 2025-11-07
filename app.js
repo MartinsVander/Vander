405270ba2023fcc56c13021de39c04295b053358
Vander <129402129+MartinsVander@users.noreply.github.com>
2025-11-07 11:47:32 -0300

    Menu - Foto

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+nomeArray.includes('Nome')
+nomeArray.indexOf()
+
+let nome = ['Jonas','Jonas Alves', 'Juca']
+ 
+
+ for(let i = 0;i<nome.length;i++){
+  if(nome[i].includes('Alves')){
+    console.log(i+':'+nome[i])
+  }
+}
+
+function nomeFuncao(){
+  //codigo
+}
+
+
+function nomeDaFuncao(parametro1, parametro2) {
+  // código
+  return valorDeRetorno;
+}
+
+function nomeDaFuncao() {
+  // código
+  return valorDeRetorno;
+}
+
+function saudacao(nome) {    
+	return "Olá, " + nome + "!";
+}
+console.log(saudacao("João")); 
+
+function calculaArea(largura, altura) {    
+	return largura * altura;
+}
+
+let area = calculaArea(10, 5);
+console.log(area); 
+
+
+function saudacao(nome, idade) { 
+	console.log("Olá, "+ nome +"! Você tem" +	idade + " anos.");
+}
+saudacao("Alice", 30); 
+
+
+
